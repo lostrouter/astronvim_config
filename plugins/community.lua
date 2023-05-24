ebbc6c1c19ef648b52b15adcd8956d903bea14cc
michael steinberg <michael.steinberg@dexcarehealth.com>
2023-05-23 23:26:30 -0700

    feat: colors

--- plugins/community.lua
+++ plugins/community.lua
@@ -12,4 +12,12 @@ return {
       disabled_filetypes = { "help" },
     },
   },
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+        require("monokai-pro").setup {
+        filter = "octagon", -- classic | octagon | pro | machine | ristretto | spectrum
+    }
+    end
+  },
 }
